ee66b318ae427bb0cb6df6aedc144fc9e436dc45
HUNGNV10-D5\HungNV10 <hung.nv155790hust@gmail.com>
2023-04-28 13:33:42 +0700

    feat(common): add conversation component

---
+++ src/components/common/Conversation.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="c-openchat__box__info">
+    <ul>
+      <li class="c-bubble">
+        <img class="c-bubble__image" src="" alt="" />
+        <p class="c-bubble__msg">
+          I want you to design something for me<span class="c-bubble__timestamp"
+            >2020-08-20 15:30:00</span
+          >
+        </p>
+      </li>
+      <li class="c-bubble my-message">
+        <img class="c-bubble__image" src="" alt="" />
+        <p class="c-bubble__msg">
+          Cool, what did you have in mind?<span class="c-bubble__timestamp"
+            >2020-08-20 15:32:00</span
+          >
+        </p>
+      </li>
+      <li class="c-bubble">
+        <img class="c-bubble__image" src="" alt="" />
+        <p class="c-bubble__msg">
+          I need a new website for my shop<span class="c-bubble__timestamp"
+            >2020-08-20 15:50:00</span
+          >
+        </p>
+      </li>
+      <li class="c-bubble my-message">
+        <img class="c-bubble__image" src="" alt="" />
+        <p class="c-bubble__msg">
+          Cool, how much have you got for a budget?<span
+            class="c-bubble__timestamp"
+            >2020-08-20 17:30:00</span
+          >
+        </p>
+      </li>
+      <li class="c-bubble">
+        <img class="c-bubble__image" src="" alt="" />
+        <p class="c-bubble__msg">
+          I will pay you in exposure mate!<span class="c-bubble__timestamp"
+            >2020-08-20 18:21:00</span
+          >
+        </p>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Conversation",
+};
+</script>
+
+<style></style>
--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -1,10 +1,11 @@
 <script lang="ts">
 import ChatSpace from "../components/common/ChatSpace.vue";
+import Conversation from "../components/common/Conversation.vue";
 import NavBar from "../components/common/NavBar.vue";
 import SearchBar from "../components/common/SearchBar.vue";
 import SideBar from "../components/common/SideBar.vue";
 export default {
-  components: { SideBar, SearchBar, ChatSpace, NavBar },
+  components: { SideBar, SearchBar, ChatSpace, NavBar, Conversation },
   setup() {
     return {};
   },
@@ -18,6 +19,7 @@ export default {
     <section class="c-openchat">
       <nav-bar />
       <chat-space />
+      <conversation />
     </section>
   </main>
 </template>
