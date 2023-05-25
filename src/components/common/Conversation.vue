f6e9110b68b0e1f2e88f47f2a180938375207187
HUNGNV10-D5\HungNV10 <hung.nv155790hust@gmail.com>
2023-05-25 16:48:06 +0700

    fix(scrollToBottom)

--- src/components/common/Conversation.vue
+++ src/components/common/Conversation.vue
@@ -54,16 +54,18 @@
         {{ item.name }}
       </div>
     </div> -->
-    <ul v-bind="wrapperProps">
+
+    <ul v-bind="wrapperProps" ref="listContainerRef">
       <li
         class="c-bubble"
-        v-for="(item, index) in list"
-        :key="item.id"
+        v-for="{ index, data } in list"
+        :key="data.id"
         :class="index % 2 ? 'my-message' : ''"
       >
         <img class="c-bubble__image" src="" alt="" />
         <p class="c-bubble__msg">
-          I want you to design something for me<span class="c-bubble__timestamp"
+          {{ data.name }}I want you to design something for me<span
+            class="c-bubble__timestamp"
             >2020-08-20 15:30:00</span
           >
         </p>
@@ -73,12 +75,13 @@
 </template>
 
 <script>
-import { ref } from "vue";
+import { onMounted, reactive, ref } from "vue";
 import { useVirtualList } from "@vueuse/core";
 export default {
   name: "Conversation",
   setup() {
-    const data = ref([
+    const listContainerRef = ref(null);
+    const data = [
       { id: 1, name: "Item 1" },
       { id: 2, name: "Item 2" },
       { id: 3, name: "Item 3" },
@@ -94,92 +97,71 @@ export default {
       { id: 13, name: "Item 13" },
       { id: 14, name: "Item 14" },
       { id: 15, name: "Item 15" },
-      { id: 15, name: "Item 15" },
-      { id: 15, name: "Item 15" },
-      { id: 15, name: "Item 15" },
-      { id: 15, name: "Item 15" },
-      { id: 15, name: "Item 15" },
-      { id: 15, name: "Item 15" },
-      { id: 15, name: "Item 15" },
-      { id: 15, name: "Item 15" },
-      { id: 15, name: "Item 15" },
-      { id: 15, name: "Item 15" },
-      { id: 15, name: "Item 15" },
-      { id: 15, name: "Item 15" },
-      { id: 15, name: "Item 15" },
-      { id: 15, name: "Item 15" },
-      { id: 15, name: "Item 15" },
-      { id: 15, name: "Item 15" },
-      { id: 15, name: "Item 15" },
-      { id: 15, name: "Item 15" },
-      { id: 15, name: "Item 15" },
-      { id: 15, name: "Item 15" },
-      { id: 15, name: "Item 15" },
-      { id: 15, name: "Item 15" },
-      { id: 15, name: "Item 15" },
-      { id: 15, name: "Item 15" },
-      { id: 15, name: "Item 15" },
-      { id: 15, name: "Item 15" },
-      { id: 15, name: "Item 15" },
-      { id: 15, name: "Item 15" },
-      { id: 15, name: "Item 15" },
-      { id: 15, name: "Item 15" },
-      { id: 15, name: "Item 15" },
-      { id: 15, name: "Item 15" },
-      { id: 15, name: "Item 15" },
-      { id: 15, name: "Item 15" },
-      { id: 15, name: "Item 15" },
-      { id: 15, name: "Item 15" },
-      { id: 15, name: "Item 15" },
-      { id: 15, name: "Item 15" },
-      { id: 15, name: "Item 15" },
-      { id: 15, name: "Item 15" },
-      { id: 15, name: "Item 15" },
-      { id: 15, name: "Item 15" },
-      { id: 15, name: "Item 15" },
-      { id: 15, name: "Item 15" },
-      { id: 15, name: "Item 15" },
-      { id: 15, name: "Item 15" },
-      { id: 15, name: "Item 15" },
-      { id: 15, name: "Item 15" },
-      { id: 15, name: "Item 15" },
-      { id: 15, name: "Item 15" },
-      { id: 15, name: "Item 15" },
-      { id: 15, name: "Item 15" },
-      { id: 15, name: "Item 15" },
-      { id: 15, name: "Item 15" },
-      { id: 15, name: "Item 15" },
-      { id: 15, name: "Item 15" },
-      { id: 15, name: "Item 15" },
-      { id: 15, name: "Item 15" },
-      { id: 15, name: "Item 15" },
-      { id: 15, name: "Item 15" },
-      { id: 15, name: "Item 15" },
-      { id: 15, name: "Item 15" },
-      { id: 15, name: "Item 15" },
-      { id: 15, name: "Item 15" },
-      { id: 15, name: "Item 15" },
-      { id: 15, name: "Item 15" },
-      { id: 15, name: "Item 15" },
-      { id: 15, name: "Item 15" },
-    ]);
-
+      { id: 16, name: "Item 15" },
+      { id: 17, name: "Item 15" },
+      { id: 18, name: "Item 15" },
+      { id: 19, name: "Item 15" },
+      { id: 20, name: "Item 15" },
+      { id: 21, name: "Item 15" },
+      { id: 22, name: "Item 15" },
+      { id: 23, name: "Item 15" },
+      { id: 24, name: "Item 15" },
+      { id: 25, name: "Item 15" },
+    ];
+    const state = reactive({
+      data: [
+        { id: 1, name: "Item 1" },
+        { id: 2, name: "Item 2" },
+        { id: 3, name: "Item 3" },
+        { id: 4, name: "Item 4" },
+        { id: 5, name: "Item 5" },
+        { id: 6, name: "Item 6" },
+        { id: 7, name: "Item 7" },
+        { id: 8, name: "Item 8" },
+        { id: 9, name: "Item 9" },
+        { id: 10, name: "Item 10" },
+        { id: 11, name: "Item 11" },
+        { id: 12, name: "Item 12" },
+        { id: 13, name: "Item 13" },
+        { id: 14, name: "Item 14" },
+        { id: 15, name: "Item 15" },
+        { id: 16, name: "Item 15" },
+        { id: 17, name: "Item 15" },
+        { id: 18, name: "Item 15" },
+        { id: 19, name: "Item 15" },
+        { id: 20, name: "Item 15" },
+        { id: 21, name: "Item 15" },
+        { id: 22, name: "Item 15" },
+        { id: 23, name: "Item 15" },
+        { id: 24, name: "Item 15" },
+        { id: 25, name: "Item 15" },
+      ],
+    });
     // Số lượng item hiển thị trên màn hình
     const itemHeight = 80;
     const itemsPerRow = 12;
     const visibleCount = 12;
 
     // Sử dụng `useVirtualList` để tạo danh sách ảo
-    const { containerProps, wrapperProps, list } = useVirtualList(data, {
-      itemHeight,
-      itemsPerRow,
-      visibleCount,
+    const { containerProps, wrapperProps, list, scrollTo } = useVirtualList(
+      state.data,
+      {
+        itemHeight,
+        itemsPerRow,
+        visibleCount,
+      }
+    );
+    onMounted(() => {
+      // scroll to bottom of screen
+      scrollTo(2000);
     });
-
     return {
       containerProps,
       wrapperProps,
+      state,
+      data,
       list,
+      listContainerRef,
     };
   },
 };
